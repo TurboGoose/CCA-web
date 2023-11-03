2c89dd436b8265b35aef7c8bcf0aadd080065b41
ilakonovalov <antonpuckich@gmail.com>
2023-11-03 18:11:16 +0300

    First prototype

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template
+from csv_reader import Csv_Reader
+
+app = Flask(__name__)
+reader = Csv_Reader("datasets/test.csv")
+reader.read_data()
+data = reader.data
+header = reader.header
+
+
+@app.route('/')
+def show_data():
+    return render_template("table.html", header=header, data=data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
