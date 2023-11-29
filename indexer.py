7a5323d181fbe3a3867a4de8957452dbf86210bd
ilakonovalov <antonpuckich@gmail.com>
2023-11-29 16:46:56 +0300

    Implement method for querying index that returns ranged list of message numbers

--- indexer.py
+++ indexer.py
@@ -1,6 +1,8 @@
 import os
 from whoosh.index import create_in, open_dir
 from whoosh.fields import *
+from whoosh.qparser import QueryParser
+
 
 class DatasetIndexer:
     def __init__(self, indices_dir):
@@ -9,22 +11,45 @@ class DatasetIndexer:
             os.mkdir(indices_dir)
 
     def index(self, index_name, dataset):
-        index_dir = os.path.join(self.indices_dir, index_name);
+        index_dir = os.path.join(self.indices_dir, index_name)
         if not os.path.exists(index_dir):
             os.mkdir(index_dir)
 
-        schema = Schema(username=TEXT, text=TEXT)
+        schema = Schema(id=NUMERIC(stored=True, unique=True, bits=64), username=TEXT, text=TEXT)
         index = create_in(index_dir, schema)
         writer = index.writer()
 
         for i, row in dataset.iterrows():
-            writer.add_document(username=row["username"], text=row["text"])
+            writer.add_document(id=i, username=row["username"], text=row["text"])
 
         writer.commit()
+
+    def search(self, index_name, query):
+        index_dir = os.path.join(self.indices_dir, index_name)
+        if not os.path.exists(index_dir):
+            raise FileNotFoundError(f"Index directory '{index_dir}' does not exist")
+        print(f"Index directory: {index_dir}")
+        index = open_dir(index_dir)
+
+        matched_docs = []
+        with index.searcher() as searcher:
+            query = QueryParser("text", index.schema).parse(query)
+            results = searcher.search(query)
+            for res in results:
+                matched_docs.append(res["id"])
+
+        return matched_docs
 
 
 if __name__ == '__main__':
     from csv_reader import CsvReader
-
     data = CsvReader("datasets/test.csv").read_data()
-    DatasetIndexer("indices").index("test", data)
+
+    indexer = DatasetIndexer("indices")
+    # indexer.index("test", data)
+
+    query = "Hello there"
+    results = indexer.search("test", query)
+
+    for id in results:
+        print(f"{id}: {data.loc[id].text}")
